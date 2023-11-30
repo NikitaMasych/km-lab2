6ed39da87bd459b56ca4db7f6b85ae7eecc8aa03
Nikita Masych <nikitamasich152@gmail.com>
2023-11-30 21:32:38 +0200

    Fixed input coordinates

--- task2.py
+++ task2.py
@@ -1,44 +1,50 @@
 import numpy as np
-from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
-# Вхідні дані (вузли інтерполяції)
-x_data = np.array([8.6, 5.8, 6.2, 6.6, 7, 7.4, 7.8, 8.2, 8.6, 9])
+from scipy.optimize import curve_fit
 
-# Різні значення y_data для лінійної і квадратичної функцій
-y_data_linear = np.array([-0.13, 0.67, 1.47, 2.27, 3.07, 3.87, 4.67, 5.47, 6.27, 7.07])
-y_data_quadratic = np.array([4.39, 3.35, 2.95, 3.19, 4.07, 5.59, 7.75, 10.55, 13.99, 18.07])
+x_data = np.array([-1.6000, -1.2000, -0.8000, -0.4000, 0, 0.4000, 0.8000, 1.2000, 1.6000, 2.0000])
 
-# Лінійна функція для апроксимації: y = A*x + B
+y_data_linear = np.array([-0.2000, 0.6000, 1.4000, 2.2000, 3.0000, 3.8000, 4.6000, 5.4000, 6.2000, 7.0000])
+y_data_quadratic = np.array([4.3200, 3.2800, 2.8800, 3.1200, 4.0000, 5.5200, 7.6800, 10.4800, 13.9200, 18.0000])
+
+k2 = 7
+k1 = k2 * 0.01
+
+x_data += k2
+
+y_data_linear += k1
+y_data_quadratic += k1
+
 def linear_func(x, A, B):
     return A * x + B
 
-# Квадратична функція для апроксимації: y = A*x^2 + B*x + C
 def quadratic_func(x, A, B, C):
     return A * x**2 + B * x + C
 
-# Апроксимація лінійною функцією
 params_linear, covariance_linear = curve_fit(linear_func, x_data, y_data_linear)
 A_linear, B_linear = params_linear
 
-# Апроксимація квадратичною функцією
 params_quadratic, covariance_quadratic = curve_fit(quadratic_func, x_data, y_data_quadratic)
 A_quadratic, B_quadratic, C_quadratic = params_quadratic
 
-# Генерація значень для побудови кривих
 x_vals = np.linspace(min(x_data), max(x_data), 100)
+
 y_linear = linear_func(x_vals, A_linear, B_linear)
 y_quadratic = quadratic_func(x_vals, A_quadratic, B_quadratic, C_quadratic)
 
-# Виведення результатів
 print("Лінійна апроксимація: y = {:.4f} * x + {:.4f}".format(A_linear, B_linear))
 print("Квадратична апроксимація: y = {:.4f} * x^2 + {:.4f} * x + {:.4f}".format(A_quadratic, B_quadratic, C_quadratic))
 
-# Побудова графіків
 plt.scatter(x_data, y_data_linear, label='Лінійні дані', color='black')
 plt.scatter(x_data, y_data_quadratic, label='Квадратичні дані', color='red')
+
+print(x_data)
+print(y_data_linear)
+
 plt.plot(x_vals, y_linear, label='Лінійна апроксимація', linestyle='dashed', color='black')
 plt.plot(x_vals, y_quadratic, label='Квадратична апроксимація', linestyle='dashed', color='red')
+
 plt.legend()
 plt.xlabel('x')
 plt.ylabel('y')
